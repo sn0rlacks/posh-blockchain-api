dc47668063451a2afd0fd331fa48cd365091bde3
sn0re <josh@chrest.cloud>
2022-02-17 15:16:59 -0600

    add Get-NFTCollectionStats
    it does it yep

---
+++ Public/Get-NFTCollectionStats.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    Pulls escrow data for a given collection on Magic Eden marketplace.ME Rate Limit is 120 Calls/Minute
+.PARAMETER CollectionName
+    ToLower, Under-delimited name of the NFT Collection. i.e high_roller_hippo_clique
+#>
+function Get-NFTCollectionStats {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string] 
+        $CollectionName
+    )
+
+    $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+    $headers.Add("Content-Type", "application/json")
+
+    try {
+        $api = "$MEUrl/rpc/getCollectionEscrowStats/$CollectionName"
+        $response = Invoke-RestMethod $api -Method 'GET' -Headers $headers -ResponseHeadersVariable 'ResponseHeaders'
+        $response | Add-Member -MemberType NoteProperty -Name 'Headers' -Value $ResponseHeaders
+        return $response
+    } catch {
+        Write-Host "Status Code:" $_.Exception.Response.StatusCode.value__
+        Write-Host "Status Description:" $_.Exception.Response.StatusDescription
+        Write-Host "Status Message:" $_.ErrorDetails.Message
+        Write-Host "Other:" $_ 
+    }
+}
