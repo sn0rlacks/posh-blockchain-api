ac3833422e5d93512998e1408b73b0c6bbec0f4b
sn0re <josh@chrest.cloud>
2022-02-17 18:12:03 -0600

    clean

--- Public/Get-NFTCollectionStats.ps1
+++ Public/Get-NFTCollectionStats.ps1
@@ -21,8 +21,10 @@ function Get-NFTCollectionStats {
         $response.results.floorPrice = $response.results.floorPrice/$Lamports
         $response.results.avgPrice24hr = [Math]::Round(($response.results.avgPrice24hr/$Lamports),2)
         $response.results.volumeAll = [Math]::Round(($response.results.volumeAll/$Lamports),2)
+        $response.results.volume24hr = [Math]::Round(($response.results.volume24hr/$Lamports),2)
+        $response.results.listedTotalValue = [Math]::Round(($response.results.listedTotalValue/$Lamports),2)
         Write-Host "Rate limit remaining for Magic Eden API: "$response.Headers['X-RateLimit-Remaining']
-        return $response.results | Select-Object symbol, floorPrice, avgPrice24hr, listedCount, volumeAll | Format-List
+        return $response.results | Select-Object symbol, floorPrice, avgPrice24hr, listedCount, volume24hr, volumeAll, listedTotalValue, availableAttribues | Format-List
     } catch {
         Write-Host "Status Code:" $_.Exception.Response.StatusCode.value__
         Write-Host "Status Description:" $_.Exception.Response.StatusDescription
